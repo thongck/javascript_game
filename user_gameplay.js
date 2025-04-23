02aab33774405da96b3eba8a901a91e263ab7223
thongck <thong_cheekang@hotmail.com>
2025-04-23 14:38:49 +0800

    set user input to lower case

--- user_gameplay.js
+++ user_gameplay.js
@@ -5,7 +5,7 @@ while(input !== 'q') {
     console.log("(w)up, (s)down, (a)left, (d)right, (q)uit.");
     input = prompt("Which way would you like to move?");
 
-    switch(input) {
+    switch(input.toLowerCase()) {
         case "w":
             console.log("You moved up.\n");
             break;
